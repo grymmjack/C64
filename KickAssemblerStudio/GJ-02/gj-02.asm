1af9953a0335b0991a52feb24336bb40f0b53baa
grymmjack <grymmz0r@yahoo.com>
2022-02-21 00:18:22 -0500

    Added fillchar.asm

--- KickAssemblerStudio/GJ-02/gj-02.asm
+++ KickAssemblerStudio/GJ-02/gj-02.asm
@@ -1,16 +1,21 @@
 BasicUpstart2(start)
 
+* = $1000
+
+.var screen = $0400
+
 start:
-    lda #$00
+    lda #1
+    ldx #0
 
 loop:
-    sta $0400,x
-    cpx #$3FF
-// will break
-.break
-    beq end
+    sta screen,x        // sta $0400,x
+    sta screen+250,x    // sta $04FA,x
+    sta screen+500,x    // sta $05F4,x
+    sta screen+750,x    // sta $06EE,x
     inx
-    jmp loop
+    cpx #250            // cpx $FA
+    bne loop
 
 end:
     rts
--- KickAssemblerStudio/gj-02.vs
+++ KickAssemblerStudio/gj-02.vs
@@ -1,5 +1,4 @@
-al C:810 .loop
-al C:80e .start
+al C:1004 .loop
+al C:1000 .start
 al C:80b .upstartEnd
-al C:81b .end
-break 815 
+al C:1015 .end
